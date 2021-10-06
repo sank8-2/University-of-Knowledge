986a917d1ff164e5bb71ddb55ebe621fd77d5fdc
Sanketh Kumar <sankethkumarbg1995@gmail.com>
2021-10-06 10:39:18 +0530

    Changes

--- form-handler.php
+++ form-handler.php
@@ -1,8 +1,24 @@
 <?php
-$name=$_POST[''];
-$visitor_email=$_POST[''];
-$subject=$_POST[''];
-$message=$_POST[''];
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
 
+$email_from= 'info@knowledge.edu';
+$email_subject= 'New Form Submission';
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "Subject: $subject.\n".
+              "User Message: $message.\n";
+
+$to = 'sankethkumarbg1995@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: pages/contact.html");
 
 ?>
